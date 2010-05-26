1aa466f6109dedbee707c4956d6dec15bd381904
yuranet <yuranet@5f7a89d5-bd68-4938-9893-cab8262575d4>
2010-05-26 17:04:45 +0000

    map editor imgmap size update prompt added

--- src_android/net/yura/domination/mobile/flashgui/MiniFlashRiskAdapter.java
+++ src_android/net/yura/domination/mobile/flashgui/MiniFlashRiskAdapter.java
@@ -2,8 +2,7 @@ package net.yura.domination.mobile.flashgui;
 
 import net.yura.domination.engine.RiskListener;
 import net.yura.domination.engine.core.RiskGame;
-import net.yura.mobile.gui.components.Frame;
-import net.yura.mobile.gui.components.Panel;
+import net.yura.mobile.logging.Logger;
 
 public class MiniFlashRiskAdapter implements RiskListener {
 
@@ -25,8 +24,7 @@ public class MiniFlashRiskAdapter implements RiskListener {
 
     @Override
     public void sendMessage(String output, boolean a, boolean b) {
-        // TODO Auto-generated method stub
-
+        Logger.debug("Game: "+output);
     }
 
     // ======================= game setup =============================
@@ -34,29 +32,26 @@ public class MiniFlashRiskAdapter implements RiskListener {
     @Override
     public void addPlayer(int type, String name, int color, String ip) {
         // TODO Auto-generated method stub
-
     }
+
     @Override
     public void delPlayer(String name) {
         // TODO Auto-generated method stub
-
     }
 
     @Override
     public void showMapPic(RiskGame p) {
         // TODO Auto-generated method stub
-
     }
+
     @Override
     public void showCardsFile(String c, boolean m) {
         // TODO Auto-generated method stub
-
     }
 
     @Override
     public void startGame(boolean s) {
         // TODO Auto-generated method stub
-
     }
 
     // ========================= in game ==============================
@@ -64,84 +59,71 @@ public class MiniFlashRiskAdapter implements RiskListener {
     @Override
     public void needInput(int s) {
         // TODO Auto-generated method stub
-
     }
 
     @Override
     public void noInput() {
         // TODO Auto-generated method stub
-
     }
 
     @Override
     public void armiesLeft(int l, boolean s) {
         // TODO Auto-generated method stub
-
     }
 
     @Override
     public void openBattle(int c1num, int c2num) {
         // TODO Auto-generated method stub
-
     }
 
     @Override
     public void closeBattle() {
-
+        // TODO Auto-generated method stub
     }
 
     @Override
     public void sendDebug(String a) {
         // TODO Auto-generated method stub
-
     }
 
     @Override
     public void serverState(boolean s) {
         // TODO Auto-generated method stub
-
     }
 
     @Override
     public void setGameStatus(String state) {
         // TODO Auto-generated method stub
-
     }
 
     @Override
     public void setNODAttacker(int n) {
         // TODO Auto-generated method stub
-
     }
 
     @Override
     public void setNODDefender(int n) {
         // TODO Auto-generated method stub
-
     }
 
     @Override
     public void setSlider(int min, int c1num, int c2num) {
         // TODO Auto-generated method stub
-
     }
 
     @Override
     public void showDice(int n, boolean w) {
         // TODO Auto-generated method stub
-
     }
 
     @Override
     public void showDiceResults(int[] att, int[] def) {
         // TODO Auto-generated method stub
-
     }
 
     @Override
     public void showMessageDialog(String a) {
         // TODO Auto-generated method stub
-
     }
 
 }
