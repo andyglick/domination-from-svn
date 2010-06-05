c890dbf0582ebbe38173a510f84c621449495f51
yuranet <yuranet@5f7a89d5-bd68-4938-9893-cab8262575d4>
2010-06-05 20:41:05 +0000

    spinner works for going down

--- src_android/net/yura/domination/mobile/flashgui/MiniFlashGUI.java
+++ src_android/net/yura/domination/mobile/flashgui/MiniFlashGUI.java
@@ -1,13 +1,18 @@
 package net.yura.domination.mobile.flashgui;
 
+import java.util.Vector;
+
 import net.yura.domination.engine.Risk;
 import net.yura.domination.engine.RiskUtil;
+import net.yura.domination.engine.core.Player;
 import net.yura.domination.engine.translation.TranslationBundle;
 import net.yura.domination.mobile.MiniUtil;
 import net.yura.domination.mobile.mapchooser.MapChooser;
 import net.yura.mobile.gui.ActionListener;
 import net.yura.mobile.gui.ButtonGroup;
+import net.yura.mobile.gui.ChangeListener;
 import net.yura.mobile.gui.components.Button;
+import net.yura.mobile.gui.components.Component;
 import net.yura.mobile.gui.components.Frame;
 import net.yura.mobile.gui.components.Label;
 import net.yura.mobile.gui.components.OptionPane;
@@ -16,7 +21,7 @@ import net.yura.mobile.gui.components.Spinner;
 import net.yura.mobile.gui.layout.XULLoader;
 import net.yura.mobile.util.Properties;
 
-public class MiniFlashGUI extends Frame {
+public class MiniFlashGUI extends Frame implements ChangeListener {
 
     private Properties resBundle = MiniUtil.wrap(TranslationBundle.getBundle());
     private Risk myrisk;
@@ -62,13 +67,7 @@ public class MiniFlashGUI extends Frame {
                 Button autoplaceall = (Button)newgame.find("autoplaceall");
                 Button recycle = (Button)newgame.find("recycle");
 
-                Label easyAI = (Label)newgame.find("easyAI");
-                Label hardAI = (Label)newgame.find("hardAI");
-                Label carpAI = (Label)newgame.find("carpAI");
-                Label human = (Label)newgame.find("human");
-
-
-                myrisk.getGame().getPlayers(); fgf
+                //myrisk.getGame().getPlayers();
 
                 myrisk.parser("startgame "+
                         GameType.getSelection().getActionCommand()+" "+
@@ -139,11 +138,108 @@ public class MiniFlashGUI extends Frame {
 
         newgame = getPanel("/newgame.xml");
 
+        addChangeListener("easyAI");
+        addChangeListener("hardAI");
+        addChangeListener("crapAI");
+        addChangeListener("human");
+
         setContentPane( new ScrollPane( newgame.getRoot() ) );
+
+        if (localgame) {
+            RiskUtil.loadPlayers( myrisk );
+        }
 
         revalidate();
         repaint();
     }
 
+    private void addChangeListener(String name) {
+        Component comp = newgame.find(name);
+        if (comp!=null && comp instanceof Spinner) {
+            ((Spinner)comp).addChangeListener(this);
+        }
+    }
+
+    public void updatePlayers() {
+
+        Vector players = myrisk.getGame().getPlayers();
+
+        int easyAIcount=0;
+        int hardAIcount=0;
+        int crapAIcount=0;
+        int humancount=0;
+
+        for (int c=0;c<players.size();c++) {
+            int type = ((Player)players.elementAt(c)).getType();
+            switch(type) {
+                case Player.PLAYER_AI_CRAP: crapAIcount++; break;
+                case Player.PLAYER_AI_HARD: hardAIcount++; break;
+                case Player.PLAYER_AI_EASY: easyAIcount++; break;
+                case Player.PLAYER_HUMAN: humancount++; break;
+            }
+        }
+
+        Component easyAI = newgame.find("easyAI");
+        Component hardAI = newgame.find("hardAI");
+        Component crapAI = newgame.find("crapAI");
+        Component human = newgame.find("human");
+
+        easyAI.setValue( new Integer(easyAIcount) );
+        hardAI.setValue( new Integer(hardAIcount) );
+        crapAI.setValue( new Integer(crapAIcount) );
+        human.setValue( new Integer(humancount) );
+    }
+
+    public void changeEvent(Component source, int num) {
+
+        Component easyAI = newgame.find("easyAI");
+        Component hardAI = newgame.find("hardAI");
+        Component crapAI = newgame.find("crapAI");
+        Component human = newgame.find("human");
+
+        int type = -1;
+        if (source == easyAI) {
+            type = Player.PLAYER_AI_EASY;
+        }
+        else if (source == hardAI) {
+            type = Player.PLAYER_AI_HARD;
+        }
+        else if (source == crapAI) {
+            type = Player.PLAYER_AI_CRAP;
+        }
+        else if (source == human) {
+            type = Player.PLAYER_HUMAN;
+        }
+
+        if (type!=-1) {
+            Vector players = myrisk.getGame().getPlayers();
+            int count=0;
+            for (int c=0;c<players.size();c++) {
+                int ptype = ((Player)players.elementAt(c)).getType();
+                if (ptype == type) {
+                    count++;
+                }
+            }
+            int newval = ((Integer)((Spinner)source).getValue()).intValue();
+
+            if (newval<count) {
+                for (int c=players.size()-1;c>=0;c--) {
+                    Player p = (Player)players.elementAt(c);
+                    if (p.getType() == type) {
+                        myrisk.parser("delplayer " + p.getName());
+                        break;
+                    }
+                }
+            }
+            else if (newval>count) {
+
+
+
+
+            }
+
+        }
+
+    }
 
 }
