3e9e0bfd6c409542064c67eeb2cfb5d1549a7d80
yuranet <yuranet@5f7a89d5-bd68-4938-9893-cab8262575d4>
2011-12-30 16:14:07 +0000

    Cache

---
+++ Domination/src/net/yura/cache/Cache.java
@@ -0,0 +1,102 @@
+package net.yura.cache;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.URLEncoder;
+
+/**
+ * @author Yura Mamyrin
+ */
+public class Cache {
+    
+    public static final boolean DEBUG = false;
+    File cacheDir;
+    
+    public Cache() {
+        
+        
+        String tmpDir = System.getProperty("java.io.tmpdir");
+        
+        String appName = "net.yura.domination"; // TODO fix
+        
+        cacheDir = new File(new File(tmpDir),appName+".cache");
+        
+        if (!cacheDir.isDirectory() && !cacheDir.mkdirs()) {
+            throw new RuntimeException("can not make cache dir: "+cacheDir);
+        }
+
+        if (DEBUG) {
+            System.out.println("[yura.net Cache] starting "+cacheDir);
+        }
+        
+    }
+    
+    private File getFileName(String uid) {
+        try {
+            String fileName = URLEncoder.encode(uid, "UTF-8");
+            return new File(cacheDir, fileName);
+        }
+        catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+        
+    }
+    
+    public void put(String key, byte[] value) {
+        File file = getFileName(key);
+        if (file.exists()) {
+            System.err.println("[yura.net Cache] already has file: "+key);
+        }
+        else {
+            try {
+                
+                if (DEBUG) {
+                    System.out.println("[yura.net Cache] saving to cache: "+key);
+                }
+                
+                FileOutputStream out = new FileOutputStream(file);
+                out.write(value);
+                out.close();
+            }
+            catch (Exception ex) {
+                if (file.exists()) {
+                    file.delete();
+                }
+                ex.printStackTrace();
+                System.err.println("[yura.net Cache] failed to save data to file: "+key);
+            }
+        }
+        
+    }
+    
+    public InputStream get(String key) {
+        File file = getFileName(key);
+        if (file.exists()) {
+            try {
+                if (DEBUG) {
+                    System.out.println("[yura.net Cache] getting from cache: "+key);
+                }
+                
+                file.setLastModified(System.currentTimeMillis());
+                return new FileInputStream(file);
+            }
+            catch (Exception ex) {
+                throw new RuntimeException(ex);
+            }
+        }
+        else {
+            if (DEBUG) {
+                System.out.println("[yura.net Cache] key not found: "+key);
+            }
+        }
+        return null;
+    }
+
+    public boolean containsKey(String key) {
+        File file = getFileName(key);
+        return file.exists();
+    }
+
+}
