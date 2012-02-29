c73cdcb7130e0703c0d0a0ab211bdac4b4a7f69c
yuranet <yuranet@5f7a89d5-bd68-4938-9893-cab8262575d4>
2012-02-29 17:45:26 +0000

    just use 1 url

--- src_mapstore/net/yura/domination/mapstore/MapUpdateService.java
+++ src_mapstore/net/yura/domination/mapstore/MapUpdateService.java
@@ -1,14 +1,16 @@
 package net.yura.domination.mapstore;
 
+import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
 import java.util.Vector;
 import net.yura.mobile.gui.Font;
 import net.yura.mobile.gui.Graphics2D;
 import net.yura.mobile.gui.border.Border;
-import net.yura.mobile.gui.border.MatteBorder;
 import net.yura.mobile.io.ServiceLink.Task;
+import net.yura.mobile.util.Url;
 
 /**
  * @author Yura
@@ -18,7 +20,7 @@ public class MapUpdateService extends Observable implements MapServerListener {
     static MapUpdateService updateService;
     
     Vector maps;
-    int results;
+    //int results;
     
     Vector mapsToUpdate = new Vector();
     MapServerClient client;
@@ -45,48 +47,64 @@ public class MapUpdateService extends Observable implements MapServerListener {
         maps = new Vector();
         client = new MapServerClient(this);
         client.start();
+        
+        String url = MapChooser.MAP_PAGE;
+        
         for (int c=0;c<mapsUIDs.size();c++) {
             String uid = (String)mapsUIDs.elementAt(c);
             Map map = MapChooser.createMap(uid);
             maps.add( map );
-            client.makeRequestXML( MapChooser.MAP_PAGE,"mapfile",uid );
+            //client.makeRequestXML( MapChooser.MAP_PAGE,"mapfile",uid );
+            
+            url = url + (url.indexOf('?')<0?'?':'&') + Url.encode("mapfile")+"="+Url.encode(uid);
         }
+        
+        client.makeRequestXML( url,null,null );
     }
 
     public void gotResultXML(String url, Task task) {
 
         Hashtable map = (Hashtable)task.getObject();
         Vector gotMaps = (Vector)map.get("maps");
-        if (gotMaps.size()==1) {
-            Map themap = (Map)gotMaps.elementAt(0);
-            String ver = themap.getVersion();
-            if (ver!=null && !"".equals(ver) && !"1".equals(ver)) {
+
+        //try { new net.yura.domination.mapstore.gen.XMLMapAccess().save(System.out, map); } catch (Exception ex) { ex.printStackTrace(); }
+        
+        for (int c=0;c<maps.size();c++) {
+            Map localMap = (Map)maps.elementAt(c);
+            List theMaps = new ArrayList(1);
+
+            for (int i=0;i<gotMaps.size();i++) {
+                Map themap = (Map)gotMaps.elementAt(i);
                 String mapUID = MapChooser.getFileUID( themap.getMapUrl() );
-                for (int c=0;c<maps.size();c++) {
-                    Map localMap = (Map)maps.elementAt(c);
-                    if (mapUID.equals( localMap.getMapUrl() )) { // we found the map
-                        if (!ver.equals( localMap.getVersion() )) { // versions do not match
-                            mapsToUpdate.add(themap);
-                            notifyListeners();
-                            //client.downloadMap( MapChooser.getURL(MapChooser.getContext(url), themap.mapUrl ) ); // download 
-                        }
-                        break;
-                    }
+                if (mapUID.equals( localMap.getMapUrl() )) { // we found the map
+                    theMaps.add(themap);
+                    // we do NOT break, just in case there is more then one
                 }
             }
+            
+            if (theMaps.size()==1) {
+                Map themap = (Map)theMaps.get(0);
+                String ver = themap.getVersion();
+                if (ver!=null && !"".equals(ver) && !"1".equals(ver) && !ver.equals( localMap.getVersion() ) ) { // versions do not match, and update is needed
+                    mapsToUpdate.add(themap);
+                    notifyListeners();
+                    //client.downloadMap( MapChooser.getURL(MapChooser.getContext(url), themap.mapUrl ) ); // download 
+                }
+            }
+            // else if 0 then we did not find it, or if more then 1 then some error has happened
+            
         }
-        // else do nothing
         
         response();
     }
 
     void response() {
-        results++;
-        if (results==maps.size()) {
+        //results++;
+        //if (results==maps.size()) {
             client.kill();
             client=null;
             maps = null;
-        }
+        //}
     }
     
     public void downloadFinished(String mapUID) {
