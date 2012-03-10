273e80bbd0fc4e7864dedf23d12981cd0d6ac254
yuranet <yuranet@5f7a89d5-bd68-4938-9893-cab8262575d4>
2012-03-10 15:04:50 +0000

    unit test for map update service

---
+++ Domination/test/junit/net/yura/domination/mapstore/MapUpdateServiceTest.java
@@ -0,0 +1,95 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.yura.domination.mapstore;
+
+import java.io.File;
+import java.util.Vector;
+import junit.framework.TestCase;
+import net.yura.domination.engine.RiskUIUtil;
+
+/**
+ *
+ * @author yuramamyrin
+ */
+public class MapUpdateServiceTest extends TestCase {
+    
+    public MapUpdateServiceTest(String testName) {
+        super(testName);
+    }
+    
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+    
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    /**
+     * Test of init method, of class MapUpdateService.
+     */
+    public void testInit() throws Exception {
+        System.out.println("init");
+
+        // TODO this test only works with the QA server
+        
+        RiskUIUtil.mapsdir = new File("./game/Domination/maps").toURI().toURL();
+        
+        Vector mapsUIDs = new Vector();
+
+        // ALL THESE MAPS NEED TO ACTUALLY BE IN THE MAPS DIR
+        mapsUIDs.add("RiskEurope.map");
+        mapsUIDs.add("ameroki.map");
+        mapsUIDs.add("eurasien.map");
+        mapsUIDs.add("geoscape.map");
+        mapsUIDs.add("lotr.map");
+        mapsUIDs.add("luca.map");
+        mapsUIDs.add("risk.map");
+        mapsUIDs.add("roman_empire.map");
+        mapsUIDs.add("sersom.map");
+        mapsUIDs.add("teg.map");
+        mapsUIDs.add("tube.map");
+        mapsUIDs.add("uk.map");
+        mapsUIDs.add("world.map");
+        
+        MapUpdateService instance = MapUpdateService.getInstance();
+        instance.init(mapsUIDs);
+        
+        Vector result = instance.mapsToUpdate;
+        
+        Vector check = new Vector();
+        check.add("ameroki.map");
+        
+        testResultContains(result,check);
+        
+    }
+
+    private void testResultContains(Vector result, Vector check) {
+        assertEquals( check.size(), result.size() );
+        
+        for (int c=0;c<check.size();c++) {
+            
+            String name = (String)check.get(c);
+            boolean found=false;
+            
+            for (int a=0;a<result.size();a++) {
+                Map map = (Map)result.get(a);
+                
+                if (map.getMapUrl().endsWith("/"+name)) {
+                    found = true;
+                }
+            }
+            
+            if (!found) {
+                fail("not found: "+name);
+            }
+            
+        }
+        
+    }
+
+}
