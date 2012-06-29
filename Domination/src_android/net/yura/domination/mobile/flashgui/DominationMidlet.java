a1e826eb8fa4aa0da942b85b575e0557460bd827
yuranet <yuranet@5f7a89d5-bd68-4938-9893-cab8262575d4>
2012-06-29 14:58:57 +0000

    only popup warn in debug

--- Domination/src_android/net/yura/domination/mobile/flashgui/DominationMidlet.java
+++ Domination/src_android/net/yura/domination/mobile/flashgui/DominationMidlet.java
@@ -1,5 +1,11 @@
 package net.yura.domination.mobile.flashgui;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
@@ -34,24 +40,26 @@ public class DominationMidlet extends Midlet {
 
             CoreUtil.setupLogging();
 
-            Logger.getLogger("").addHandler( new Handler() {
-                boolean open;
-                @Override
-                public void publish(LogRecord record) {
-                    if (record.getLevel().intValue() >= Level.WARNING.intValue()) {
-                        if (!open) {
-                            open = true;
-                            try {
-                                OptionPane.showMessageDialog(null, record.getMessage()+" "+record.getThrown(), "WARN", OptionPane.WARNING_MESSAGE);
+            if ( "true".equals( System.getProperty("debug") ) ) {
+                Logger.getLogger("").addHandler( new Handler() {
+                    boolean open;
+                    @Override
+                    public void publish(LogRecord record) {
+                        if (record.getLevel().intValue() >= Level.WARNING.intValue()) {
+                            if (!open) {
+                                open = true;
+                                try {
+                                    OptionPane.showMessageDialog(null, record.getMessage()+" "+record.getThrown(), "WARN", OptionPane.WARNING_MESSAGE);
+                                }
+                                catch(Exception ex) { ex.printStackTrace(); }
                             }
-                            catch(Exception ex) { ex.printStackTrace(); }
                         }
                     }
-                }
 
-                @Override public void flush() { }
-                @Override public void close() { }
-            } );
+                    @Override public void flush() { }
+                    @Override public void close() { }
+                } );
+            }
             
             // if we want to see DEBUG, default is INFO
             //java.util.logging.Logger.getLogger("").setLevel(java.util.logging.Level.ALL);
@@ -101,6 +109,59 @@ public class DominationMidlet extends Midlet {
         //risk.parser("newplayer ai hard green greg");
         //risk.parser("startgame domination increasing");
 
+        
+        try {
+            //File saves = new File( net.yura.android.AndroidMeApp.getIntance().getFilesDir() ,"saves");
+            //File sdsaves = new File("/sdcard/Domination-saves");
+            //copyFolder(saves, sdsaves);
+            //copyFolder(sdsaves, saves);
+            //System.out.println("files"+ Arrays.asList( saves.list() ) );
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
     }
 
+    public static void copyFolder(File src, File dest) throws IOException{
+     
+            if(src.isDirectory()){
+     
+                    //if directory not exists, create it
+                    if(!dest.exists()){
+                       dest.mkdir();
+                       System.out.println("Directory copied from " 
+                                  + src + "  to " + dest);
+                    }
+     
+                    //list all the directory contents
+                    String files[] = src.list();
+     
+                    for (String file : files) {
+                       //construct the src and dest file structure
+                       File srcFile = new File(src, file);
+                       File destFile = new File(dest, file);
+                       //recursive copy
+                       copyFolder(srcFile,destFile);
+                    }
+     
+            }else{
+                    //if file, then copy it
+                    //Use bytes stream to support all file types
+                    InputStream in = new FileInputStream(src);
+                    OutputStream out = new FileOutputStream(dest); 
+     
+                    byte[] buffer = new byte[1024];
+     
+                    int length;
+                    //copy the file content in bytes 
+                    while ((length = in.read(buffer)) > 0){
+                       out.write(buffer, 0, length);
+                    }
+     
+                    in.close();
+                    out.close();
+                    System.out.println("File copied from " + src + " to " + dest);
+            }
+    }
 }
