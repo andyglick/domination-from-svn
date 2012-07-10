6d2380b62e627dbe3bc457b3b8a0343c4778efe4
yuranet <yuranet@5f7a89d5-bd68-4938-9893-cab8262575d4>
2012-07-10 21:59:58 +0000

    can change stats view

--- Domination/src_android/net/yura/domination/android/StatsActivity.java
+++ Domination/src_android/net/yura/domination/android/StatsActivity.java
@@ -1,8 +1,8 @@
 package net.yura.domination.android;
 
+import java.util.HashMap;
 import java.util.List;
 import net.yura.android.AndroidMeApp;
-import net.yura.domination.engine.Risk;
 import net.yura.domination.engine.core.Player;
 import net.yura.domination.mobile.flashgui.DominationMain;
 import net.yura.domination.mobile.flashgui.GameActivity;
@@ -16,27 +16,93 @@ import org.achartengine.renderer.XYMultipleSeriesRenderer;
 import org.achartengine.renderer.XYSeriesRenderer;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class StatsActivity extends Activity {
 
     Properties resb = GameActivity.resb;
     
-    Risk getRisk() {
+    public interface EnumConverter<T> {
+        public T convert();
+    }
+    
+    public class ReverseEnumMap<T,V extends Enum<V> & EnumConverter<T>> extends HashMap<T, V> {
+        public ReverseEnumMap(Class<V> valueType) {
+            for (V v : valueType.getEnumConstants()) {
+                put(v.convert(), v);
+            }
+        }
+    }
+
+    
+    
+
+    enum Stat implements EnumConverter<Integer> {
+        COUNTRIES("countries",1),
+        ARMIES("armies",2),
+        KILLS("kills",3),
+        CASUALTIES("casualties",4),
+        REINFORCEMENTS("reinforcements",5),
+        CONTINENTS("continents",6),
+        EMPIRE("empire",7),
+        ATTACKS("attacks",8),
+        RETREATS("retreats",9),
+        VICTORIES("victories",10),
+        DEFEATS("defeats",11),
+        ATTACKED("attacked" ,12);
+
+        private final String name;
+        private final int id;
+        Stat(String name, int id) {
+            this.name = name;
+            this.id = id;
+        }
+        @Override
+        public Integer convert() {
+            return id;
+        }
+    }
+    
+    final ReverseEnumMap<Integer,Stat> lookup = new ReverseEnumMap<Integer,Stat>(Stat.class);
+    
+    List<Player> getPlayersStats() {
         DominationMain dmain = (DominationMain)AndroidMeApp.getMIDlet();
-        return dmain.risk;
+        return dmain.risk.getGame().getPlayersStats();
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
         setTitle( resb.getString("swing.tab.statistics") );
+        showGraph( Stat.COUNTRIES );
+    }
 
-        showGraph(1);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        for (Stat s:Stat.values()) {
+            MenuItem item = menu.add( android.view.Menu.NONE, s.id, android.view.Menu.NONE, resb.getString("swing.toolbar." + s.name ) );
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        
+        int id = item.getItemId();
+        
+        Stat stat = lookup.get( id );
+        if (stat!=null) {
+            showGraph(stat);
+        }
+        
+        return true;
     }
     
-    public void showGraph(int a) {
-        GraphicalView gview = ChartFactory.getLineChartView(this, getDataset(a), getRenderer() );
+    public void showGraph(Stat a) {
+        GraphicalView gview = ChartFactory.getLineChartView(this, getDataset(a.id), getRenderer() );
         setContentView(gview);
     }
     
@@ -44,7 +110,7 @@ public class StatsActivity extends Activity {
         
         XYMultipleSeriesRenderer renderer = new XYMultipleSeriesRenderer();
         
-        List<Player> players = getRisk().getGame().getPlayersStats();
+        List<Player> players = getPlayersStats();
 
         for (int c = 0; c < players.size(); c++) {
         
@@ -62,7 +128,7 @@ public class StatsActivity extends Activity {
 
         XYMultipleSeriesDataset dataset = new XYMultipleSeriesDataset();
         
-        List<Player> players = getRisk().getGame().getPlayersStats();
+        List<Player> players = getPlayersStats();
 
         //draw each player graph.
         for (int c = 0; c < players.size(); c++) {
