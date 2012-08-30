5af749329db9ab94e1ba1059a583d150c7008524
yuranet <yuranet@5f7a89d5-bd68-4938-9893-cab8262575d4>
2012-08-30 17:32:00 +0000

    mini lobby

---
+++ Domination/src_mapstore/net/yura/domination/lobby/mini/GameRenderer.java
@@ -0,0 +1,52 @@
+package net.yura.domination.lobby.mini;
+
+import net.yura.lobby.model.Game;
+import net.yura.mobile.gui.Font;
+import net.yura.mobile.gui.Graphics2D;
+import net.yura.mobile.gui.cellrenderer.DefaultListCellRenderer;
+import net.yura.mobile.gui.components.Component;
+
+public class GameRenderer extends DefaultListCellRenderer {
+
+    MiniLobbyClient lobby;
+    
+    Game game;
+    
+    public GameRenderer(MiniLobbyClient l) {
+        lobby = l;
+    }
+    
+    public Component getListCellRendererComponent(Component list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+        Component c = super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+
+        game = (Game)value;
+        
+        setIcon( lobby.getIconForGame(game) );
+
+        return c;
+    }
+
+    public void paintComponent(Graphics2D g) {
+        super.paintComponent(g);
+
+        g.setColor( getForeground() );
+
+        Font font = g.getFont();
+        String action;
+
+        switch (game.getState()) {
+            case Game.STATE_CAN_JOIN: action = "Join"; break;
+            case Game.STATE_CAN_LEAVE: action = "Leave"; break;
+            case Game.STATE_CAN_PLAY: action = "Play"; break;
+            case Game.STATE_CAN_WATCH: action = "Watch"; break;
+            default: action = null; break;
+        }
+
+        if (action!=null) {
+            g.drawString(action, getWidth() - font.getWidth(action), (getHeight()-font.getHeight())/2);
+        }
+        
+    }
+    
+    
+}
