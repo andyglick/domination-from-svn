f01c7e36c10ed39d307e746d03159c211416852c
yuranet <yuranet@5f7a89d5-bd68-4938-9893-cab8262575d4>
2012-09-16 15:35:22 +0000

    mini lobby logic split

---
+++ src_mapstore/net/yura/domination/lobby/mini/MiniLobbyGame.java
@@ -0,0 +1,25 @@
+package net.yura.domination.lobby.mini;
+
+import net.yura.lobby.model.Game;
+import net.yura.lobby.model.GameType;
+import net.yura.mobile.gui.Icon;
+import net.yura.mobile.util.Properties;
+
+/**
+ * @author Yura Mamyrin
+ */
+public interface MiniLobbyGame {
+
+    void addLobbyGameMoveListener(MiniLobbyClient lgl);
+    
+    public Properties getProperties();
+
+    public boolean isMyGameType(GameType gametype);
+    public Icon getIconForGame(Game game);
+    
+    public void openGameSetup(GameType gameType);
+    
+    public void objectForGame(Object object);
+    public void stringForGame(String message);
+
+}
