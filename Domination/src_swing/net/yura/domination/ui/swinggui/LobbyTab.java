b92291815e31f847366701575a0ffe9f97769cfd
yuranet <yuranet@5f7a89d5-bd68-4938-9893-cab8262575d4>
2012-10-31 19:00:16 +0000

    lobby in SwingGUI

---
+++ Domination/src_swing/net/yura/domination/ui/swinggui/LobbyTab.java
@@ -0,0 +1,107 @@
+package net.yura.domination.ui.swinggui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JMenu;
+import javax.swing.JToolBar;
+import javax.swing.SwingUtilities;
+import net.yura.domination.engine.Risk;
+import net.yura.domination.engine.RiskUIUtil;
+import net.yura.domination.lobby.mini.MiniLobbyRisk;
+import net.yura.lobby.mini.MiniLobbyClient;
+import net.yura.lobby.model.Game;
+import net.yura.lobby.model.GameType;
+import net.yura.me4se.ME4SEPanel;
+import net.yura.mobile.gui.components.Label;
+
+/**
+ * @author Yura Mamyrin
+ */
+public class LobbyTab extends ME4SEPanel implements SwingGUITab,ActionListener {
+
+    MiniLobbyClient mlc;
+    Risk risk;
+
+    JToolBar toolbar;
+    JButton start,stop;
+    
+    public LobbyTab(Risk myrisk) {
+        getApplicationManager().applet = RiskUIUtil.applet;
+        risk = myrisk;
+
+        toolbar = new JToolBar();
+        toolbar.setRollover(true);
+	toolbar.setFloatable(false);
+
+        start = new JButton("Start Lobby");
+	start.setActionCommand("start");
+	start.addActionListener(this);
+	toolbar.add(start);
+
+        stop = new JButton("Stop Lobby");
+	stop.setActionCommand("stop");
+	stop.addActionListener(this);
+	toolbar.add(stop);
+        
+        updateButton();
+        
+        add( new Label("click start in the toolbar") );
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        String action = e.getActionCommand();
+        if ("start".equals(action)) {
+            createLobby();
+            updateButton();
+        }
+        else {
+            closeLobby();
+            updateButton();
+        }
+    }
+    
+    private void updateButton() {
+        start.setEnabled( mlc==null );
+        stop.setEnabled( mlc!=null );
+    }
+
+    public JToolBar getToolBar() {
+        return toolbar;
+    }
+    public JMenu getMenu() {
+        return null;
+    }
+    public String getName() {
+        return "Lobby";
+    }
+
+
+
+    void createLobby() {
+        
+        mlc = new MiniLobbyClient( new MiniLobbyRisk(risk) {
+            private net.yura.domination.lobby.client.GameSetupPanel gsp;
+            public void openGameSetup(GameType gameType) {
+                if (gsp==null) {
+                    gsp = new net.yura.domination.lobby.client.GameSetupPanel();
+                }
+                Game result = gsp.showDialog( SwingUtilities.getWindowAncestor(LobbyTab.this) , gameType.getOptions(), lobby.whoAmI() );
+                if (result!=null) {
+                    lobby.createNewGame(result);
+                }
+            }
+        } );
+
+        add( mlc.getRoot() );
+        
+    }
+    
+    void closeLobby() {
+        mlc.destroy();
+        mlc = null;
+
+        add( new Label("no lobby") );
+    }
+    
+}
