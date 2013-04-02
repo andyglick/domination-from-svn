2aa18f27095b61382a29f6c293301171c5d7b026
yuranet <yuranet@5f7a89d5-bd68-4938-9893-cab8262575d4>
2013-04-02 22:38:11 +0000

    show missions button correctly if default cards file is changed

--- src/net/yura/domination/engine/core/RiskGame.java
+++ src/net/yura/domination/engine/core/RiskGame.java
@@ -2801,7 +2801,7 @@ System.out.print(str+"]\n");
     
     private void readObject(java.io.ObjectInputStream in) throws IOException, ClassNotFoundException {
     	in.defaultReadObject();
-    	if (this.mapfile != null) {
+    	if (this.mapfile != null && gameState!=STATE_NEW_GAME) {
             try {
                     loadMap(false, null);
             }
