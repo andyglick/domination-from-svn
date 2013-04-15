1d8b499c1f0386f9e6971bd9630c4bbae5c0b199
yuranet <yuranet@5f7a89d5-bd68-4938-9893-cab8262575d4>
2013-04-15 01:48:33 +0000

    bug fixes

--- Domination/src_mapstore/net/yura/swingme/core/CoreUtil.java
+++ Domination/src_mapstore/net/yura/swingme/core/CoreUtil.java
@@ -8,6 +8,9 @@ import net.yura.mobile.util.Properties;
  */
 public class CoreUtil {
 
+    /**
+     * @see net.yura.mobile.gui.layout.XULLoader#getPropertyText(java.lang.String, boolean) 
+     */
     public static Properties wrap(final ResourceBundle res) {
         return new Properties() {
             public String getProperty(String key) {
@@ -15,16 +18,17 @@ public class CoreUtil {
                     return res.getString(key);
                 }
                 catch (Exception ex) {
-                    return null;
+                    // sometimes this method is used by the XULLoader, but sometimes it is used directly
+                    // from code, thats why for those cases we should not ever return null, as a sring is expected
+                    ex.printStackTrace();
+                    return "???"+key+"???";
                 }
             }
         };
     }
 
     public static void setupLogging() {
-
         net.yura.mobile.logging.Logger.setLogger( new net.yura.swingme.core.J2SELogger() );
-
     }
 
 }
