d717365690b1fb99ee42835a2686944a8dce5def
yuranet <yuranet@5f7a89d5-bd68-4938-9893-cab8262575d4>
2013-06-30 12:22:45 +0000

    broekn saves fix

--- Domination/src_android/net/yura/domination/android/GameActivity.java
+++ Domination/src_android/net/yura/domination/android/GameActivity.java
@@ -1,6 +1,7 @@
 package net.yura.domination.android;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -9,20 +10,22 @@ import net.yura.android.AndroidMeApp;
 import net.yura.android.AndroidPreferences;
 import net.yura.domination.engine.Risk;
 import net.yura.domination.engine.RiskUtil;
+import net.yura.domination.engine.core.Player;
+import net.yura.domination.engine.core.RiskGame;
 import net.yura.domination.mobile.flashgui.DominationMain;
 import net.yura.mobile.logging.Logger;
 
 public class GameActivity extends AndroidMeActivity {
-    
+
     @Override
     protected void onSingleCreate() {
-        
+
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
         DominationMain.appPreferences = new AndroidPreferences(preferences);
-        
+
         super.onSingleCreate();
     }
-    
+
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
@@ -33,28 +36,46 @@ public class GameActivity extends AndroidMeActivity {
             // in game thread, we do not want to do it there as we will not know when its finished
             //getRisk().parser("savegame "+getAutoSaveFileURL());
 
-            Thread thread = new Thread(null,null,"Domination-onSaveInstanceState", 100000000) {
-                @Override
-                public void run() {
-                    // in current thread
-                    try {
-                        // we want to save to auto.save.part and then rename it to auto.save
-                        // in case the save fails for some reason so we dont end up with half a game in the file
-                        RiskUtil.saveFile(DominationMain.getAutoSaveFileURL()+".part" , getRisk().getGame() );
-                        File autoSaveFile = DominationMain.getAutoSaveFile();
-                        RiskUtil.rename(new File(autoSaveFile.getParent(),autoSaveFile.getName()+".part"), autoSaveFile);
-                    }
-                    catch (Throwable ex) {
-                        ex.printStackTrace();
-                    }
+            final File autoSaveFile = DominationMain.getAutoSaveFile();
+            final File tempSaveFile = new File(autoSaveFile.getParent(),autoSaveFile.getName()+".part");
+            final Risk risk = getRisk();
+            final RiskGame game = risk.getGame();
+
+            // we can not save the current state if the AI is playing as we will get a inconsistent state inside the game
+            if (game.getCurrentPlayer()==null || game.getCurrentPlayer().getType()==Player.PLAYER_HUMAN) {
+                // we need to make a new as the main android thread does not have a big enough stack
+                Thread thread = new Thread(null,null,"Domination-onSaveInstanceState", 100000000) {
+                    @Override
+                    public void run() {
+                        // in current thread
+                        try {
+                            // we want to save to auto.save.part and then rename it to auto.save
+                            // in case the save fails for some reason so we dont end up with half a game in the file
+                            RiskUtil.saveFile(DominationMain.getAutoSaveFileURL()+".part" , game );
+                            RiskUtil.rename(tempSaveFile, autoSaveFile);
+                        }
+                        catch (Throwable ex) {
+                            ex.printStackTrace();
+                        }
+                    };
                 };
-            };
-            thread.start();
-            try {
-                thread.join();
+                thread.start();
+                tryAndWaitForThreadToFinish(thread);
             }
-            catch(InterruptedException in) {
-                Thread.currentThread().interrupt();
+            else {
+                try {
+                    byte[] data = risk.getLastSavedState();
+                    if (data.length==0) {
+                        throw new IllegalStateException("data can not have zero length");
+                    }
+                    FileOutputStream out = new FileOutputStream(tempSaveFile);
+                    out.write(data);
+                    out.close();
+                    RiskUtil.rename(tempSaveFile, autoSaveFile);
+                }
+                catch (Throwable ex) {
+                    ex.printStackTrace();
+                }
             }
         }
     }
@@ -84,4 +105,12 @@ public class GameActivity extends AndroidMeActivity {
         return dmain==null?null:dmain.risk;
     }
 
+    private static void tryAndWaitForThreadToFinish(Thread thread) {
+        try {
+            thread.join();
+        }
+        catch(InterruptedException in) {
+            Thread.currentThread().interrupt();
+        }
+    }
 }
