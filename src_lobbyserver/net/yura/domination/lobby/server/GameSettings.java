2cfb2bd9881630c0ce507a8b10f6a29803b1badc
yuranet <yuranet@5f7a89d5-bd68-4938-9893-cab8262575d4>
2013-12-27 19:25:00 +0000

    checking on finished state of a game

--- src_lobbyserver/net/yura/domination/lobby/server/GameSettings.java
+++ src_lobbyserver/net/yura/domination/lobby/server/GameSettings.java
@@ -2,22 +2,28 @@ package net.yura.domination.lobby.server;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 import net.yura.domination.engine.ai.AIManager;
+import net.yura.domination.engine.core.RiskGame;
 
 /**
  * @author Yura Mamyrin
  */
 public class GameSettings implements GameSettingsMXBean {
 
+    @Override
     public void setAIWait(int a) {
         AIManager.setWait(a);
     }
 
+    @Override
     public int getAIWait() {
         return AIManager.getWait();
     }
 
+    @Override
     public void saveGame(int id) throws Exception {
 
         Set<Thread> threadSet = Thread.getAllStackTraces().keySet();
@@ -38,4 +44,22 @@ public class GameSettings implements GameSettingsMXBean {
         throw new IllegalArgumentException("game "+id+" not found");
     }
 
+    @Override
+    public List<Integer> markFinished() throws Exception {
+
+        Set<Thread> threadSet = Thread.getAllStackTraces().keySet();
+        Thread[] threadArray = threadSet.toArray(new Thread[threadSet.size()]);
+        List<Integer> games = new ArrayList();
+        for (Thread thread: threadArray) {
+            if (thread instanceof ServerRisk) {
+                ServerRisk risk =(ServerRisk)thread;
+
+                if (!risk.sgr.isFinished() && risk.getGame().getState() == RiskGame.STATE_GAME_OVER) {
+                    risk.sgr.gameFinished("ERROR");
+                    games.add(risk.sgr.getId());
+                }
+            }
+        }
+        return games;
+    }
 }
