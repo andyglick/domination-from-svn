484bce28e17839b13ceb77b33b16ac70c95c19de
yuranet <yuranet@5f7a89d5-bd68-4938-9893-cab8262575d4>
2014-07-16 23:09:37 +0000

    player list started

---
+++ Domination/src_android/net/yura/domination/mobile/flashgui/PlayerList.java
@@ -0,0 +1,40 @@
+package net.yura.domination.mobile.flashgui;
+
+import net.yura.mobile.gui.components.List;
+import net.yura.mobile.gui.components.Component;
+import net.yura.mobile.gui.cellrenderer.DefaultListCellRenderer;
+import net.yura.domination.engine.core.RiskGame;
+import net.yura.domination.engine.core.Player;
+
+/**
+ * @author Yura
+ */
+public class PlayerList extends List {
+
+    private RiskGame game;
+
+    public PlayerList() {
+        setLayoutOrientation(HORIZONTAL);
+        setCellRenderer(new DefaultListCellRenderer() {
+            public Component getListCellRendererComponent(Component list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+                Component component = super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+                Player player = (Player) value;
+                setBackground(player.getColor());
+                return component;
+            }
+        });
+    }
+
+    public void setGame(RiskGame game) {
+        this.game = game;
+    }
+
+    public int getSize() {
+        return game.getPlayers().size();
+    }
+
+    public Object getElementAt(int index) {
+        return game.getPlayers().get(index);
+    }
+
+}
