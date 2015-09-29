04609c0f1c27b05b81c26fbd4fa4a68eb53f38fe
yuranet <yuranet@5f7a89d5-bd68-4938-9893-cab8262575d4>
2015-09-29 21:40:42 +0000

    can open game by id

--- src_swing/net/yura/domination/ui/swinggui/LobbyTab.java
+++ src_swing/net/yura/domination/ui/swinggui/LobbyTab.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionListener;
 import java.util.ResourceBundle;
 import javax.swing.JButton;
 import javax.swing.JMenu;
+import javax.swing.JOptionPane;
 import javax.swing.JToolBar;
 import javax.swing.SwingUtilities;
 import net.yura.domination.engine.Risk;
@@ -12,6 +13,7 @@ import net.yura.domination.engine.RiskUIUtil;
 import net.yura.domination.engine.SwingMEWrapper;
 import net.yura.domination.engine.translation.TranslationBundle;
 import net.yura.lobby.mini.MiniLobbyClient;
+import net.yura.lobby.model.Game;
 import net.yura.me4se.ME4SEPanel;
 import net.yura.mobile.gui.components.Label;
 
@@ -26,7 +28,7 @@ public class LobbyTab extends ME4SEPanel implements SwingGUITab,ActionListener {
     Risk risk;
 
     JToolBar toolbar;
-    JButton start,stop;
+    JButton start,stop,open;
     
     public LobbyTab(Risk myrisk) {
         getApplicationManager().applet = RiskUIUtil.applet;
@@ -45,7 +47,13 @@ public class LobbyTab extends ME4SEPanel implements SwingGUITab,ActionListener {
 	stop.setActionCommand("stop");
 	stop.addActionListener(this);
 	toolbar.add(stop);
-        
+
+        open = new JButton("Open Game");
+	open.setActionCommand("open");
+	open.addActionListener(this);
+        open.setEnabled(false);
+	toolbar.add(open);
+
         updateButton();
         
         add( new Label("click start in the toolbar") );
@@ -57,9 +65,30 @@ public class LobbyTab extends ME4SEPanel implements SwingGUITab,ActionListener {
             createLobby();
             updateButton();
         }
-        else {
+        else if ("stop".equals(action)) {
             closeLobby();
             updateButton();
+        }
+        else if ("open".equals(action)) {
+            String input = JOptionPane.showInputDialog("game id:");
+            if (input != null) {
+                try {
+                    int id = Integer.parseInt(input);
+                    Game game = mlc.findGame(id);
+                    if (game != null) {
+                        mlc.playGame(game);
+                    }
+                    else {
+                        JOptionPane.showMessageDialog(this, "game " + id + " not found");
+                    }
+                }
+                catch(NumberFormatException ex) {
+                    JOptionPane.showMessageDialog(this, input + " not a number");
+                }
+            }
+        }
+        else {
+            throw new RuntimeException("unknown action " + action);
         }
     }
     
@@ -84,12 +113,14 @@ public class LobbyTab extends ME4SEPanel implements SwingGUITab,ActionListener {
         mlc = SwingMEWrapper.makeMiniLobbyClient(risk, SwingUtilities.getWindowAncestor(this) );
         mlc.removeBackButton();
         add( mlc.getRoot() );
+        open.setEnabled(true);
     }
     
     void closeLobby() {
         mlc.destroy();
         mlc = null;
         add( new Label("no lobby") ); // can throw when DesktopPane is null (open and close MapStore)
+        open.setEnabled(false);
     }
 
 }
